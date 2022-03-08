ee7e88f08556489aa13fb135fcd2cb15d9e52a6c
noverd <71782778+noverd@users.noreply.github.com>
2022-03-08 10:53:50 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='scratchclient',
+      version='5.2',
+      description='Gagarinten`s ScratchClient Edition',
+      packages=['scratchclient'],
+      author_email='mihajlovic.aleksa@gmail.com',
+      zip_safe=False)
